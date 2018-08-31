d67215846f94d56f4c222905205ee016e0808b76
dijahp <dijah@bellsouth.net>
2018-08-30 22:07:21 -0400

    First Commit

---
+++ app/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+// Require and Load Libs
+require_once 'library/Core.php';
+require_once 'library/Controller.php';
+require_once 'library/Database.php';
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+    require_once '../app/bootstrap.php';
+
+    //Init Core Lib
+
+    $init = new Core;
